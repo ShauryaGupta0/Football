28d190893e0fda975ba40ac05fe2b26c06d29945
ShauryaGupta0 <168058242+ShauryaGupta0@users.noreply.github.com>
2024-05-12 09:53:57 +0530

    Update Loader.php
    Changed use pocketmine\Player; to use pocketmine\player\Player;
    Changed $this->getServer()->getLevels() to $this->getServer()->getWorldManager()->getWorlds()
    Changed $sender->getLevel() to $sender->getWorld()
    Changed $sender->getLevel()->broadcastLevelSoundEvent() to $sender->getWorld()->addSound()

--- src/xBeastMode/Football/Loader.php
+++ src/xBeastMode/Football/Loader.php
@@ -6,76 +6,76 @@ namespace xBeastMode\Football;
 
 use pocketmine\entity\Entity;
 use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\utils\TextFormat;
 
 class Loader extends PluginBase{
-	public function onEnable() : void{
-		Entity::registerEntity(BallEntity::class, true);
-		$this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
-	}
+    public function onEnable() : void{
+        Entity::registerEntity(BallEntity::class, true);
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+    }
 
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-		switch($command->getName()){
-                        case "killballs":
-                                $count = 0;
-                                foreach($this->getServer()->getLevels() as $level){
-                                        foreach($level->getEntities() as $entity){
-                                                if($entity instanceof BallEntity){
-                                                        $entity->close();
-                                                        $count++;
-                                                }
-                                        }
-                                }
-                                $sender->sendMessage(TextFormat::GREEN . "Cleared " . TextFormat::YELLOW . $count . TextFormat::GREEN . " balls.");
-                                return true;
-			case "addball":
-			        if($sender instanceof Player){
-                                        if(count($args) > 0){
-                                                switch(strtolower($args[0])){
-                                                        case "small":
-                                                                $nbt = Entity::createBaseNBT($sender->asVector3());
-                                                                $ent = Entity::createEntity("BallEntity", $sender->level, $nbt);
-                                                                $sender->sendMessage(TextFormat::GREEN . "Added new small ball.");
-                                                                $ent->spawnToAll();
-                                                                $sender->level->broadcastLevelSoundEvent($sender, LevelSoundEventPacket::SOUND_BLOCK_END_PORTAL_FRAME_FILL);
-                                                                break;
-                                                        case "medium":
-                                                                $nbt = Entity::createBaseNBT($sender->asVector3());
-                                                                $ent = Entity::createEntity("BallEntity", $sender->level, $nbt);
-                                                                $sender->sendMessage(TextFormat::GREEN . "Added new medium ball.");
-                                                                $ent->setScale(2);
-                                                                $ent->spawnToAll();
-                                                                $sender->level->broadcastLevelSoundEvent($sender, LevelSoundEventPacket::SOUND_BLOCK_END_PORTAL_FRAME_FILL);
-                                                                break;
-                                                        case "big":
-                                                                $nbt = Entity::createBaseNBT($sender->asVector3());
-                                                                $ent = Entity::createEntity("BallEntity", $sender->level, $nbt);
-                                                                $sender->sendMessage(TextFormat::GREEN . "Added new big ball.");
-                                                                $ent->setScale(3);
-                                                                $ent->spawnToAll();
-                                                                $sender->level->broadcastLevelSoundEvent($sender, LevelSoundEventPacket::SOUND_BLOCK_END_PORTAL_FRAME_FILL);
-                                                                break;
-                                                        default:
-                                                                $sender->sendMessage(TextFormat::RED . "Ball not found. Please choose: " . TextFormat::YELLOW . "small, medium, big");
-                                                                break;
-                                                }
-                                        }else{
-                                                $nbt = Entity::createBaseNBT($sender->asVector3());
-                                                $ent = Entity::createEntity("BallEntity", $sender->level, $nbt);
-                                                $sender->sendMessage(TextFormat::GREEN . "Added new small ball.");
-                                                $ent->spawnToAll();
-                                                $sender->level->broadcastLevelSoundEvent($sender, LevelSoundEventPacket::SOUND_BLOCK_END_PORTAL_FRAME_FILL);
-                                        }
-                                }else{
-			                $sender->sendMessage("Use command in-game.");
-                                }
-				return true;
-			default:
-				return false;
-		}
-	}
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+        switch($command->getName()){
+            case "killballs":
+                $count = 0;
+                foreach($this->getServer()->getWorldManager()->getWorlds() as $level){
+                    foreach($level->getEntities() as $entity){
+                        if($entity instanceof BallEntity){
+                            $entity->flagForDespawn();
+                            $count++;
+                        }
+                    }
+                }
+                $sender->sendMessage(TextFormat::GREEN . "Cleared " . TextFormat::YELLOW . $count . TextFormat::GREEN . " balls.");
+                return true;
+            case "addball":
+                if($sender instanceof Player){
+                    if(count($args) > 0){
+                        switch(strtolower($args[0])){
+                            case "small":
+                                $nbt = Entity::createBaseNBT($sender->getPosition());
+                                $ent = Entity::createEntity("BallEntity", $sender->getWorld(), $nbt);
+                                $sender->sendMessage(TextFormat::GREEN . "Added new small ball.");
+                                $ent->spawnToAll();
+                                $sender->getWorld()->addSound($sender->getPosition(), LevelSoundEventPacket::SOUND_BLOCK_END_PORTAL_FRAME_FILL);
+                                break;
+                            case "medium":
+                                $nbt = Entity::createBaseNBT($sender->getPosition());
+                                $ent = Entity::createEntity("BallEntity", $sender->getWorld(), $nbt);
+                                $sender->sendMessage(TextFormat::GREEN . "Added new medium ball.");
+                                $ent->setScale(2);
+                                $ent->spawnToAll();
+                                $sender->getWorld()->addSound($sender->getPosition(), LevelSoundEventPacket::SOUND_BLOCK_END_PORTAL_FRAME_FILL);
+                                break;
+                            case "big":
+                                $nbt = Entity::createBaseNBT($sender->getPosition());
+                                $ent = Entity::createEntity("BallEntity", $sender->getWorld(), $nbt);
+                                $sender->sendMessage(TextFormat::GREEN . "Added new big ball.");
+                                $ent->setScale(3);
+                                $ent->spawnToAll();
+                                $sender->getWorld()->addSound($sender->getPosition(), LevelSoundEventPacket::SOUND_BLOCK_END_PORTAL_FRAME_FILL);
+                                break;
+                            default:
+                                $sender->sendMessage(TextFormat::RED . "Ball not found. Please choose: " . TextFormat::YELLOW . "small, medium, big");
+                                break;
+                        }
+                    }else{
+                        $nbt = Entity::createBaseNBT($sender->getPosition());
+                        $ent = Entity::createEntity("BallEntity", $sender->getWorld(), $nbt);
+                        $sender->sendMessage(TextFormat::GREEN . "Added new small ball.");
+                        $ent->spawnToAll();
+                        $sender->getWorld()->addSound($sender->getPosition(), LevelSoundEventPacket::SOUND_BLOCK_END_PORTAL_FRAME_FILL);
+                    }
+                }else{
+                    $sender->sendMessage("Use command in-game.");
+                }
+                return true;
+            default:
+                return false;
+        }
+    }
 }
